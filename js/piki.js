969d25cd75e674d4eed54f5a4ac8d9c1eee62f8c
Doron Brikman <brikmand@gmail.com>
2014-11-12 20:04:09 +0200

    timer directive direct to website

--- js/piki.js
+++ js/piki.js
@@ -5,7 +5,35 @@ app.controller('questionCtrl', function(){
 
     var that = this;
     this.nextQuestion = function () {
-        that.number++
+        that.number++;
     };
 });
 
+app.directive('timer', ['$interval', function ($interval) {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            interval: '@'
+        },
+        templateUrl: 'templates/timer.html',
+        link: function(scope) {
+
+            scope.minutes = Math.floor(scope.interval / 60);
+            scope.seconds = scope.interval % 60 < 10 ? '0' + scope.interval % 60 : scope.interval % 60;
+
+            function update() {
+                scope.interval--;
+
+                if (scope.interval <= 0) {
+                    $interval.cancel(cancel);
+                }
+
+                scope.minutes = Math.floor(scope.interval / 60);
+                scope.seconds = scope.interval % 60 < 10 ? '0' + scope.interval % 60 : scope.interval % 60;
+            };
+
+            var cancel = $interval(update, 1000);
+        }
+    }
+}]);
