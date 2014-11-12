3d4489fbd5a8d871defeff3b3ac27d340066994e
Eyal Ofri <eyal1990@gmail.com>
2014-11-12 19:27:52 +0200

    design + change pages

--- js/piki.js
+++ js/piki.js
@@ -1,6 +1,11 @@
-var app = angular.module('piki');
+var app = angular.module('piki', []);
 
 app.controller('questionCtrl', function(){
-    this.number = 2;
+    this.number = 1;
+
+    var that = this;
+    this.nextQuestion = function () {
+        that.number++
+    };
 });
 
