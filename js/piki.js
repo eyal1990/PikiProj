5c184530afe1ddda78a2a46fe2f51d84feff11d3
Doron Brikman <brikmand@gmail.com>
2014-11-16 16:03:38 +0200

    scroll down feature

--- js/piki.js
+++ js/piki.js
@@ -1,5 +1,12 @@
 var app = angular.module('piki', ['ngAnimate']);
 
+app.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+}]);
+
 app.controller('questionCtrl', function(){
     this.number = 1;
 
@@ -31,20 +38,30 @@ app.directive('timer', ['$interval', function ($interval) {
 
                 scope.minutes = Math.floor(scope.interval / 60);
                 scope.seconds = scope.interval % 60 < 10 ? '0' + scope.interval % 60 : scope.interval % 60;
-            };
+            }
 
             var cancel = $interval(update, 1000);
         }
     }
 }]);
 
-app.controller('listCtrl', function() {
-});
+app.controller('listCtrl', ['$location', '$anchorScroll', function($location, $anchorScroll) {
+    this.scrollDown = function () {
+
+        $location.hash('bottom');
+
+        $anchorScroll();
+    }
+}]);
 
 app.directive('progressbar', ['$interval', function($interval){
     return {
         restrict: 'E',
         replace: true,
+        scope: {
+            percentages: '=',
+            done: '&'
+        },
         templateUrl: 'templates/progressbar.html',
         link: function(scope, element) {
 
@@ -52,15 +69,16 @@ app.directive('progressbar', ['$interval', function($interval){
             scope.percentages = 0;
 
             function update() {
-                scope.percentages += 10;
+                scope.percentages += 1;
                 progress.css('width', scope.percentages + '%');
 
                 if (scope.percentages === 100) {
                     $interval.cancel(cancel);
+                    scope.done();
                 }
             }
 
-            var cancel = $interval(update, 500);
+            var cancel = $interval(update, 50);
         }
     }
 }]);
