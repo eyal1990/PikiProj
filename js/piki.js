f6006ac80d38a3bdfa56eb7e3f489bc0c6fcb3f9
Doron Brikman <brikmand@gmail.com>
2014-11-16 09:47:51 +0200

    Progress bar

--- js/piki.js
+++ js/piki.js
@@ -1,4 +1,4 @@
-var app = angular.module('piki', []);
+var app = angular.module('piki', ['ngAnimate']);
 
 app.controller('questionCtrl', function(){
     this.number = 1;
@@ -34,6 +34,33 @@ app.directive('timer', ['$interval', function ($interval) {
             };
 
             var cancel = $interval(update, 1000);
+        }
+    }
+}]);
+
+app.controller('listCtrl', function() {
+});
+
+app.directive('progressbar', ['$interval', function($interval){
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'templates/progressbar.html',
+        link: function(scope, element) {
+
+            var progress = angular.element(element.children()[0]);
+            scope.percentages = 0;
+
+            function update() {
+                scope.percentages += 10;
+                progress.css('width', scope.percentages + '%');
+
+                if (scope.percentages === 100) {
+                    $interval.cancel(cancel);
+                }
+            }
+
+            var cancel = $interval(update, 500);
         }
     }
 }]);
